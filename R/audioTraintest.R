258e29834a1306e2c8d8daa55befa88536443740
aayushiaggarwal05 <aggarwalaayushi17@gmail.com>
2018-04-25 13:57:25 +0530

    function

--- R/audioTraintest.R
+++ R/audioTraintest.R
@@ -1,99 +1,55 @@
-#import audioFeatureExtraction as aF
-
-featureAndTrain<-function(listOfDirs, mtWin, mtStep, stWin, stStep, modelName, computeBEAT=False, perTrain=0.90)
+library(pdist)
+classify<-function(X,Y,k,testSample)
 {
-k=list(features=features,classnames=classnames,filenames=filenames)
-k=aF.dirsWavFeatureExtraction(listOfDirs, mtWin, mtStep, stWin, stStep, computeBEAT=computeBEAT)
-if(length(k$features)==0)
-{
-  print ("trainSVM_feature ERROR: No data found in any input folder!")
-  return
-}
-numoffeatures=NCOL(k$features)
-featurenames<-c()
-
-for (i in 1:numoffeatures)
-{
-  featurenames[i]=cat(" features",i)
   
-}
-
-writeTrainDataToARFF(modelName, features, classNames, featureNames)
-
-#write this function
-x=c(lapply(features,length))
-for (i in 1:length(x))
-{li
-  
-  if (x[i] ==0)
-    print (cat("trainSVM_feature ERROR: ",listOfDirs[i]," folder is empty or non-existing!"))
-} #check for values , error in function
-
-classifierParams=c(1, 3, 5, 7, 9, 11, 13, 15)        
-
-features2 = list()
-for (f in features)
-     {
-       fTemp=list()
-       for(i in NROW(features))
-       {
-         temp = f[i,]
-       }
-       
-     }
-  
-
-
-
-
-
-}
-
-m = array(1:60, dim=c(3,4,5))
-l=NCOL(m)
-l
-k=list(a=a,b=b,l=l)
-a=c(1,2)
-b=c(9,10)
-l=c(4,7)
-print(k$b)
-
-print(length(b))
-
-
-
-
-features=list('a'=c(1,2,3,4),'b'=c(3,4,5,6,6,7),'c'=c(4,2,7,5,9,8,0,0,2))
-
-for (f in features)
-{
-  fTemp=list()
-
-  for(i in 1:length(features))
+  nClasses1=unique(Y)
+  nClasses=NROW(nClasses1)
+  i=NCOL(testSample)
+  n=nrow(X)
+  m=nrow(Y)
+  Yi=matrix(testSample,nrow=1,ncol=i)
+  D <- sqrt(matrix(rep(apply(X *X, 1, sum), n), m, n, byrow = F) + matrix(rep(apply(Y*Y, 1, sum), m), m, n, byrow = T) - 2 *X %*% t(Y))
+  D
+  #YD=pdist(X,Yi)
+  YDT=as.matrix(YD)
+  YDist=t(YDT)
+  iSort=order(YDist)
+  iSort
+  P=matrix(0,nClasses,1)
+  P
+  for(i in seq(length(nClasses)))
   {
-    
-    y=c(lapply(features,length))
-    for (j in 1:length(y[i]))
-    {temp=c()
-    temp = (features[i])
-    if((!any(is.nan(temp)))&(!any(is.infinite(temp))))
-    { append(fTemp,toList(temp))}
-    else
-    {
-        print ("NaN Found! Feature vector not used for training")
-    }
-    append(features2,fTemp)
-         
-    }
-   
+    y=iSort[1]
+    print(y)
+    z=which(Y[1,1:k]==i,arr.ind=T)
+    z1=NROW(z)
+    P[i]=z1/k
+    print(P[i])
   }
-  features = features2
-    
-  }
+  maxarg=which.max(P)
+  l=list()
+  l$maxarg=maxarg
+  l$P=P
+  return (l)
   
+}
 
-
-
+nClasses=10
+vector1 <- c(5,9,3)
+vector2 <- c(10,11,12,13,14,15)
+X<- array(c(vector1,vector2),dim = c(3,3))
+vect1 <- c(51,91,31)
+vect2 <- c(101,111,121,131,141,151)
+Y<- array(c(vect1,vect2),dim = c(3,3))
+Y
+v1 <- c(50,90,30)
+v2 <- c(100,110,120,130,140,150)
+k=3
+testSample<- array(c(v1,v2),dim = c(3,3,2))
+ret=list(maxarg=maxarg,P=P)
+ret=classify(X,Y,k,test)
+print(ret$maxarg)
+print(ret$P)
 
 
 
