51356f57ed153c358c77691b1f05c6e363b3ee63
paruljindal <jindalparul1996@gmail.com>
2018-04-29 20:25:00 +0530

    #problems in evaluate classifier(because of randsplitfunction)

--- R/audioTraintest_classificationwrapper.R
+++ R/audioTraintest_classificationwrapper.R
@@ -2,10 +2,10 @@ classifierWrapper<-function(X,Y,k,testSample)
 {
   R=-1
   P=-1
-  ret=list(R=maxarg,P=P)
-  ret=classify(X,Y,k,testSample)
+  retclassifierWrapper=list(R=maxarg,P=P)
+  retclassifierWrapper=classify(X,Y,k,testSample)
   
-  return (ret)
+  return (retclassifierWrapper)
 }
 
 trainKNN<-function(features,k)
@@ -107,6 +107,12 @@ evaluateClassifier<-function(features,ClassNames,nExp,Params,parameterMode,perTr
 {
   #check return tye variables in list
   #returnnormalizefeatures=list(featuresNorm=featuresNorm,meanreturn=meanreturn,stdreturn=stdreturn)=normalizeFeatures(features)
+  x=matrix(c(1,2,3,4,5,6),ncol=2)
+  z=matrix(c(11,12,13,14,15,16),ncol=2)
+  w=matrix(c(21,22,23,24,25,26),ncol=2)
+  Params =c(1, 3, 5, 7, 9, 11, 13, 15)  
+  ClassNames=c("abc","bcd","pqr")
+  features=list(x,z,w)
   nClasses=length(features)
   CALL=c()
   acALL=c()
@@ -117,6 +123,7 @@ evaluateClassifier<-function(features,ClassNames,nExp,Params,parameterMode,perTr
   F1ClassesAll=c()
   CMsAll=c()
   nSampleTotal=0
+  nExp=20
   for (f in features)
     {
     nSampleTotal=nSampleTotal+NROW(f)
@@ -145,48 +152,214 @@ evaluateClassifier<-function(features,ClassNames,nExp,Params,parameterMode,perTr
       {
         nTestSamples=NROW(featuresTest[c1])#check NROW or length
         Results=matrix(0,nTestSamples,1)
+        retclassifierWrapper=list()
+        retclassifierWrapper=list(R=maxarg,P=P)
         for (ss in seq(1,nTestSamples))
         {
-          #continue
+          retclassifierWrapper=classifierWrapper(returnlistOfFeatures2Matrix$X,returnlistOfFeatures2Matrix$y,C,features[c1,ss])
+          Results[ss]=retclassifierWrapper$R
         }
+        for (c2 in seq(1,nClasses))
+        {
+          CMt[c1,c2]=length(which(Results==c2,arr.ind = TRUE))
         }
+      }
+    }
+      CM+CM+CMt
+      CM=CM+0.0000000010
+      Rec=matrix(0,NROW(CM),1)
+      Pre=matrix(0,NROW(CM),1)
+      for (ci in seq(1,NROW(CM)))
+      {
+        Rec[ci]=CM[ci,ci]/sum(CM[ci,1:NCOL(CM)])
+        Pre[ci]=CM[ci,ci]/sum(CM[1:NROW(CM),ci])
+      }
+      PrecisionClassesAll=c(PrecisionClassesAll,Pre)
+      RecallClassesAll=c(RecallClassesAll,Rec)
+      F1=(2*Rec*Pre)/(Rec+Pre)
+      F1ClassesAll=c(F1ClassesAll,F1)
+      l1=sum(diag(CM))
+      l2=l1/sum(CM)
+      acAll=c(acAll,l2)
+      CMsAll=c(CMsAll,CM)
+      F1ALL=c(F1ALL,mean(F1))
+      
+    
+  }
+  print("\t\t")
+  i=1
+  for (c in ClassNames)
+  {
+    if(i==length(ClassNames)-1)
+    {
+      #print("{0:s}\t\t".format(c))
+    }
+    else
+    {
+      #print("{0:s}\t\t".format(c))
+    }
+    print("OVERALL")
+    print("\tC")
+    for (c in ClassNames)
+    {
+      print("\tPRE\tREC\tF1")
+    }
+    #print("\t{0:s}\t{1:s}".format("ACC", "F1"))
+    bestAcInd=which.max(acALL)
+    bestF1Ind=which.max(F1ALL)
+    for (i in seq(1,length(PrecisionClassesAll)))
+    {
+      # print "\t{0:.3f}".format(Params[i]),
+      for (c in seq(1,length(PrecisionClassesAll[i])))
+      {
+        # print "\t{0:.1f}\t{1:.1f}\t{2:.1f}".format(100.0 * PrecisionClassesAll[i][c], 100.0 * RecallClassesAll[i][c], 100.0 * F1ClassesAll[i][c]),
+      }
+      #print "\t{0:.1f}\t{1:.1f}".format(100.0 * acAll[i], 100.0 * F1All[i]),
+      if(i==bestF1Ind)
+      {
+        print("\tbest\t")
+      }
+      if(i==bestAcInd)
+      {
+        print("\tbest\t")
+      }
+    }
+    if(parameterMode==0)
+    {
+      print("Confusion Matrix:")
+      printConfusionMatrix(CMsAll[bestAcInd],ClassNames)
+      return (Params[bestAcInd])
+    }
+    else if(parameterMode==1)
+    {
+      print("Confusion Matrix:")
+      printConfusinMatrix(CMsAll[bestF1Ind],ClassNames)
+      return (Params[bestF1Ind])
     }
   }
 }
 
 
+printConfusionMatrix<-function(CM,ClassNames)
+{
+  if(NROW(CM)!=length(ClassNames))
+  {
+    print("printConfusionMatrix: Wrong argument sizes\n")
+    return ()
+  }
+  for (c in ClassNames)
+  {
+    if(length(c)>4)
+    {
+      c=c[1:3]
+    }
+    #print "\t{0:s}".format(c),
+  }
+   for (c in ClassNames)
+   {
+     if(length(c)>4)
+     {
+       c=c[1:3]
+     }
+     #print "{0:s}".format(c),
+   }
+     for (j in seq(1,length(ClassNames)))
+     {
+       #print "\t{0:.2f}".format(100.0 * CM[i][j] / numpy.sum(CM)),
+     }
+   }
+    
+    
+  
 
 
 
 
+normalizeFeatures<-function(features)
+{
+  X=array()
+  i=1
+  for (f in features)
+  {
+    if(NROW(f)>0)
+    {
+      if(i==1)
+      {
+        X=f
+      }
+      else
+      {
+        lengthofstack=NROW(X)+NROW(f)
+        print(lengthofstack)
+        print(f)
+        X=rbind(X,f)
+        print(X)
+      }
+      i=i+1
+    }
+  }
+  MEAN=colMeans(X, na.rm = FALSE, dims = 1)+0.00000000000001
+  STD=apply(X,2,sd)+0.00000000000001
+  featuresNorm=list()
+  for (f in features)
+  {
+    
+    ft=f
+    for (nSamples in seq(1,NROW(f)))
+    {
+      
+      ft[nSamples,seq(1,NCOL(ft))]=(trunc((ft[nSamples,seq(1,NCOL(ft))]-MEAN)/STD))
+      
+    }
+    featuresNorm=list(featuresNorm,ft)
+  }
+  returnnormalizeFeatures=list(featuresNorm=featuresNorm,MEAN=MEAN,STD=STD)
+  return (returnnormalizeFeatures)
+}
+
+
+x=matrix(c(1,4,2,5,3,6),nrow=2)
+z=matrix(c(11,14,12,15,13,16),nrow=2)
+w=matrix(c(21,24,22,25,23,26),nrow=2)
+features=list(x,z,w)
+returnnormalizeFeatures=list(featuresNorm=featuresNorm,MEAN=MEAN,STD=STD)
+returnnormalizeFeatures=normalizeFeatures(features)
+print(returnnormalizeFeatures$featuresNorm)
+print(returnnormalizeFeatures$MEAN)
+print(returnnormalizeFeatures$STD)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fileClassification<-function(inputFile,modelName)
+{
+  if(!file.exists(inputFile))
+  {
+    print("wav file not found")
+  }
+  if(!file.exists(modelName))
+  {
+    print("input model not found")
+  }
+  returnloadknn=list(X=X,Y=Y,k=k,MEAN=MEAN,STD=STD,classNames=classNames,mtWin=mtWin,mtStep=mtStep,stWin=stWin,stStep=stStep,computeBEAT=computeBEAT)
+  returnloadknn=loadKNNModel(modelName)
+  returnreadaudiofile=list(Fs=Fs,x=x)
+  returnreadaudiofile=audioBasicIO.readAudioFile(inputFile)
+  returnreadaudiofile$x=audioBasicIO.readAudioFile(returnreadaudiofile$x)
+ # if isinstance(x, int):                               
+  #  return (-1, -1, -1)
+  returnnull=list(ret1=-1,ret2=-1,ret2=-1)
+  if((NROW(x)/Fs)<=mtWin)
+  {
+    return (returnnull)
+  }
+  returnFeatureExtraction=list(MidTermFeatures=MidTermFeatures,s=s)
+  returnFeatureExtraction=aF.mtfeatureExtraction(x,Fs,mtWin*Fs,mtStep,round(Fs * stWin), round(Fs * stStep))
+  returnFeaturesExtraction$MidTermFeatures=rowMeans( returnFeaturesExtraction$MidTermFeatures=rowMeans,na.rm = FALSE, dims = 1)
+  curFV=(returnFeaturesExtraction$MidTermFeatures-MEAN)/STD
+  returnClassifierWrapper=list(Result=Result,P=P)
+  returnClassifierWrapper=classifierWrapper(X,Y,k,curFV)
+  returnoffileclassification=list(Result=Result,P=P,classNames=classNames)
+  return (returnoffileclassification)
+  
+  
+  }
